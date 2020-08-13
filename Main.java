a94d6b7065b0695819f0817e8b7c91470a7b6624
kl80752 <69646991+kl80752@users.noreply.github.com>
2020-08-13 12:33:43 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Kyle Lam");
+		System.out.println("I play Minecraft among other games");
+		System.out.println("I'm not sure where to go to for college, but I do have enough saved up");
+		System.out.println("I plan on majoring in CS but I am also interested in business and economics");
+		System.out.println("I got a 5 on the AP World Exam and thats pretty cool");
+	}
+}
